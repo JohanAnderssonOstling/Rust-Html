3c946c86bf9320f298d6c8a333fb364b0ff63a94
johan <johan.andersson.ostling@protonmail.com>
2025-01-26 22:52:26 +0100

    Refactor rendering

--- src/book_elem.rs
+++ src/book_elem.rs
@@ -1,36 +1,98 @@
-use std::ops::Range;
-
-use floem::cosmic_text::{Attrs, AttrsList, TextLayout, Weight};
-use floem::kurbo::Size;
-use floem::peniko;
-use floem::peniko::Color;
-use floem::style::{FontWeight, MarginBottom};
+use std::time::Instant;
+use floem::cosmic_text::{Attrs, AttrsList, TextLayout};
+use floem::kurbo::{Point, Size};
 use floem::view::View;
 use floem::views::Decorators;
 use image::DynamicImage;
-use roxmltree::{Document, Node};
+use roxmltree::Node;
 use sha2::{Digest, Sha256};
 
+static BLOCK_ELEMENTS: [&str; 37] = [
+    "html", "body", "article", "section", "nav", "aside",
+    "h1", "h2", "h3", "h4", "h5", "h6", "hgroup", "header",
+    "footer", "address", "p", "hr", "pre", "blockquote",
+    "ol", "ul", "menu", "li", "dl", "dt", "dd", "figure",
+    "figcaption", "main", "div", "table", "form", "fieldset",
+    "legend", "details", "summary"
+];
+
+#[derive(Clone)]
 pub enum BookElem {
-    Text((TextLayout), BlockStyle),
+    Text(TextLayout),
     Img(DynamicImage, f64, Vec<u8>),
     Block(Vec<BookElem>, BlockStyle),
 }
 
-pub enum Measurement {
-    em(f32),
-    px(f32),
+#[derive(Clone)]
+pub struct BlockElem {
+    pub children: Vec<Elem>,
+    total_child_count: usize,
+}
+
+impl BlockElem {
+    pub fn add_child (&mut self, elem: Elem) {
+        match &elem.elem_type {
+            ElemType::Block(block) => {
+                self.total_child_count += block.total_child_count;
+            }
+            ElemType::Lines(_) => {self.total_child_count += 1}
+        }
+        self.children.push(elem);
+    }
+}
+
+#[derive(Clone)]
+pub struct Elem {
+    pub size: Size,
+    pub point: Point,
+    pub elem_type: ElemType
+}
+
+#[derive(Clone)]
+
+pub enum ElemType {
+    Block(BlockElem),
+    Lines(ElemLines)
+}
+#[derive(Clone)]
+
+pub struct ElemLines {
+    height: f64,
+    pub elem_lines: Vec<ElemLine>
+}
+#[derive(Clone)]
+
+pub struct ElemLine {
+    pub height: f64,
+    pub inline_elems: Vec<InlineElem>
+}
+#[derive(Clone)]
+
+pub struct InlineElem {
+    pub x: f64,
+    pub inline_content: InlineContent
+}
+
+#[derive(Clone)]
+
+pub enum InlineContent {
+    Text(TextLayout)
+}
+
+#[derive(Clone)]
+pub struct InlineItem {
+    size: Size,
+    inline_content: InlineContent
+}
+
+struct LayoutState {
+    x: f64,
+    y: f64
 }
 
 #[derive(Copy, Clone)]
 pub struct BlockStyle {
     pub margins : (f64, f64, f64, f64)
-}
-
-pub struct TextStyle {
-    font_size   : Measurement,
-    font_weight : FontWeight,
-    font_color  : Color,
 }
 
 #[derive(Copy, Clone)]
@@ -49,115 +111,110 @@ impl Default for Style {
 }
 
 pub struct BookElemFactory {
-    text_style  : Attrs<'static>,
-    header_style: Attrs<'static>,
-    small_style : Attrs<'static>,
-    link_style  : Attrs<'static>,
-    
+    curr_x: f64,
+    curr_y: f64,
 }
 
 impl BookElemFactory {
-    pub fn new(text_style: Attrs<'static>, header_style: Attrs<'static>, small_style: Attrs<'static>, link_style: Attrs<'static>) -> Self{
-        BookElemFactory {text_style, header_style, small_style, link_style}
-    }
-}
-
-impl Default for BookElemFactory {
-    fn default() -> Self {
-
-        let text_style      = Attrs::new().font_size(20.).monospaced(true);
-        let header_style    = Attrs::new().weight(Weight::BOLD).font_size(30.);
-        let small_style     = Attrs::new().font_size(17.);
-        let link_style      = Attrs::new().font_size(19.).color(Color::BLUE);
-        Self {text_style, header_style, small_style, link_style}
-    }
-}
-
-struct PElem {
-    default_style: Attrs<'static>,
-    attrs_list: AttrsList,
-    text_result: String,
-    text_length: usize,
-    book_elems: Vec<BookElem>,
-    style: Style,
-}
-
-impl PElem {
-    pub fn new(default_style: Attrs<'static>) -> Self{
-        let attrs_list = AttrsList::new(default_style);
-        let style = Style::default();
-        Self {default_style, attrs_list, text_result: String::new(), text_length: 0, book_elems: Vec::new(), style}
-    }
-    fn add_styled_text (mut self, text: &str, attrs: Attrs) -> Self{
-        let new_text_length = self.text_length + text.chars().count();
-        let range = Range {start: self.text_length, end: new_text_length};
-        self.attrs_list.add_span(range, attrs);
-        self.text_result.push_str(text);
-        self.text_length = new_text_length;
-        self
-    }
-    
-    fn add_plain_text (mut self, text: &str) -> Self {
-        self.text_length += text.chars().count();
-        self.text_result.push_str(text);
-        self
-    }
-    
-    //Empties the text added by add_plain_text and add_styled_text
-    fn empty_text(mut self, style: &Style) -> Self {
-        let mut text_layout = TextLayout::new();
-        text_layout.set_text(&self.text_result, self.attrs_list);
-        text_layout.set_size(600., f32::MAX);
-        self.book_elems.push(BookElem::Text(text_layout, self.style.block_style));
-        self.attrs_list = AttrsList::new(self.default_style);
-        self.text_result = String::new();
-        self.text_length = 0;
-        self
+    pub fn new() -> Self{
+        BookElemFactory {curr_x: 0., curr_y: 0.}
     }
 }
 
 impl BookElemFactory {
-    pub fn layout(&self, document: Document) -> Vec<BookElem>{
-        let mut pelem = PElem::new(self.text_style);
-        pelem = self.parse_node(document.root_element(), pelem, Style::default());
-        pelem = pelem.empty_text(&Style::default());
-        pelem.book_elems
-    }
 
-    fn parse_inline(&self, node: Node, mut pelem: PElem, style: Attrs) -> PElem{
-        //println!("Inline Parsing: {}\t{}l", node.tag_name().name(), node.text().unwrap_or_default());
-        if node.text().is_some() && node.tag_name().name().eq("") {pelem = pelem.add_styled_text(node.text().unwrap(), style);}
-        for child in node.children() {pelem = self.parse_inline(child, pelem, style);}
-        pelem
-    }
 
-    fn parse_node(&self, node: Node, mut pelem: PElem, mut style: Style) -> PElem {
-        //println!("Parsing: {}", node.tag_name().name());
-        match node.tag_name().name() {
-            "div"   => {pelem = pelem.empty_text(&style); pelem.style.block_style.margins.0 = 0.;}
-            "p"     => {pelem = pelem.empty_text(&style); pelem.style.block_style.margins.0 = 20.;}
-            "h1"    => {pelem = pelem.empty_text(&style); return self.parse_inline(node, pelem, self.header_style)}
-            "h2"    => {pelem = pelem.empty_text(&style); return self.parse_inline(node, pelem, self.header_style)}
-            "h3"    => {pelem = pelem.empty_text(&style); return self.parse_inline(node, pelem, self.header_style)}
-            "dl"    => {pelem = pelem.empty_text(&style);}
-            "a"     => {return self.parse_inline(node, pelem, self.link_style)}
-            "small" => {return self.parse_inline(node, pelem, self.small_style)}
-            "head"  => {return pelem;}
-            "dt"    => {}
-            "dd"    => {}
-            "span"  => {}
-            "img"   => {}
-            _ => {
-                pelem = pelem.add_plain_text(node.text().unwrap_or_default())}
-        };
-        for child in node.children() {
-            println!("{}", style.block_style.margins.0);
-            pelem = self.parse_node(child, pelem, style.clone());
+    pub fn layout_elem_lines(&mut self, inline_items: Vec<InlineItem>, width: f64) -> Elem{
+        let init_point = Point::new(self.curr_x, self.curr_y);
+        let mut elem_lines  = ElemLines {height: 0., elem_lines: Vec::new()};
+        let mut curr_line   = ElemLine  {height: 0., inline_elems: Vec::new()};
+        for inline_item in inline_items {
+            if self.curr_x + inline_item.size.width > 600. {
+                self.curr_x         = 0.;
+                self.curr_y         += curr_line.height;
+                elem_lines.height   += curr_line.height;
+                elem_lines.elem_lines.push(curr_line);
+                curr_line = ElemLine {height: 0., inline_elems: Vec::new()};
+            }
+            curr_line.height = f64::max(curr_line.height, inline_item.size.height);
+            let inline_elem = InlineElem {x: self.curr_x, inline_content: inline_item.inline_content};
+            self.curr_x += inline_item.size.width;
+            curr_line.inline_elems.push(inline_elem);
         }
-        pelem
+        self.curr_y += curr_line.height;
+        elem_lines.height += curr_line.height;
+        elem_lines.elem_lines.push(curr_line);
+
+        self.curr_x = 0.;
+        Elem {size: Size::new(width, elem_lines.height), point: init_point, elem_type: ElemType::Lines(elem_lines)}
     }
     
+    pub fn parse(&mut self, node: Node) -> Elem {
+        if node.tag_name().name().eq("html") {
+            for child in node.children() {
+                if child.tag_name().name().eq("body") { return self.parse(child); }
+            }
+        }
+        let mut block_elem = BlockElem {children: Vec::new(), total_child_count: 0};
+        let mut inline_items: Vec<InlineItem> = Vec::new();
+        let font = Attrs::new().font_size(20.);
+        let attrs_list = AttrsList::new(Attrs::new().font_size(20.));
+        let init_point = Point::new(self.curr_x, self.curr_y);
+        let mut top = 0.;
+        let mut bottom = 0.;
+        if node.tag_name().name().eq("p") {
+            top = font.font_size as f64;
+            bottom = font.font_size as f64;
+        }
+        self.curr_y += top;
+        //println!("{}", node.tag_name().name());
+        for child in node.children() {
+            let tag_name = child.tag_name().name();
+            if BLOCK_ELEMENTS.contains(&tag_name) {
+                if inline_items.len() != 0 {
+                    block_elem.add_child(self.layout_elem_lines(inline_items, 600.));
+                    inline_items = Vec::new();
+                }
+                block_elem.add_child(self.parse(child));
+            }
+            else if tag_name.eq("") { inline_items.extend(self.parse_text(child, attrs_list.clone())); }
+            else { inline_items.extend(self.parse_inline(child, attrs_list.clone())); }
+        }
+        if inline_items.len() != 0 {
+            block_elem.add_child(self.layout_elem_lines(inline_items, 600.));
+        }
+        self.curr_y += bottom;
+        let block_height = block_elem.children.iter().fold(0., |acc, elem| acc + elem.size.height);
+        Elem {size: Size::new(600., block_height + top + bottom), point: init_point, elem_type: ElemType::Block(block_elem)}
+    }
 
+    pub fn parse_inline(&self, node: Node, attrs_list: AttrsList) -> Vec<InlineItem> {
+        let mut inline_items: Vec<InlineItem> = Vec::new();
+        for child in node.children() {
+            if child.tag_name().name().eq("") {
+                inline_items.extend(self.parse_text(child, attrs_list.clone()));
+            }
+            else {
+                inline_items.extend(self.parse_inline(child, attrs_list.clone()));
+            }
+        }
+        inline_items
+    }
+
+    pub fn parse_text(&self, node: Node, attrs_list: AttrsList) -> Vec<InlineItem> {
+        let mut inline_items: Vec<InlineItem> = Vec::new();
+        if node.text().unwrap().eq("\n") {return Vec::new()}
+        let now = Instant::now();
+       for word in node.text().unwrap().split(" ") {
+            let mut text_layout = TextLayout::new();
+            text_layout.set_text(format!{"{word} "}.as_str(), attrs_list.clone());
+            let size = text_layout.size();
+            let inline_content = InlineContent::Text(text_layout);
+            inline_items.push(InlineItem {size, inline_content});
+        }
+        inline_items
+    }
+    
     pub fn img(image: DynamicImage, col_width: f32) -> BookElem{
         let aspect_ratio = image.width() as f64 / image.height() as f64;
         let mut s = Sha256::new();
@@ -165,30 +222,5 @@ impl BookElemFactory {
         let hash = s.finalize().to_vec();
         println!("Loading image");
         BookElem::Img(image, aspect_ratio, hash)
-    }
-    
-    fn create_text_layout(text: &str, attrs_list: AttrsList, col_width: f32) -> TextLayout {
-        let mut text_layout = TextLayout::new();
-        text_layout.set_size(col_width, f32::MAX);
-        text_layout.set_text(&text, attrs_list);
-        text_layout
-    }
-}
-
-impl BookElem {
-    pub fn size(&self) -> Size {
-        match self {
-            BookElem::Text(text_layout, block_style) => {text_layout.size()}
-            BookElem::Img(img, _, _) => {Size::new(img.width()as f64, img.height() as f64)}
-            _ => {Size::new(0., 0.)}
-        }
-    }
-
-    pub fn get_height(&self, col_width: f64) -> f64{
-        match self {
-            BookElem::Text(text_layout, block_style) => {text_layout.size().height}
-            BookElem::Img(_, aspect, _) => {col_width / aspect}
-            _ => {0.}
-        }
     }
 }
